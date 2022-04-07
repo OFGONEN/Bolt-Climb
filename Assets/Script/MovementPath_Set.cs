8453a8c73a62639c07e085e4db8c72b492c25505
ofgonen <ofgonen@gmail.com>
2022-04-07 16:55:56 +0300

    Implement MovementPath

---
+++ Assets/Script/MovementPath_Set.cs
@@ -0,0 +1,11 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+
+[ CreateAssetMenu( fileName = "set_movement_path", menuName = "FF/Data/Set/Movement Path" ) ]
+public class MovementPath_Set : RuntimeSet< int, Vector3[] >
+{
+}
