830d2ea60bc2e1408b1622e4def1b661bdb23222
Ömer Faruk Gönen <ofgonen@gmail.com>
2022-04-08 17:03:02 +0300

    Implement Velocity
    - Velocity scriptable object that includes data and functionlaity about velocity
    - Create asset for nut velocity

---
+++ Assets/Script/Velocity.cs
@@ -0,0 +1,60 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+using Sirenix.OdinInspector;
+
+[ CreateAssetMenu( fileName = "velocity", menuName = "FF/Data/Game/Velocity" ) ]
+public class Velocity : ScriptableObject
+{
+#region Fields
+    // Private
+	[ ShowInInspector, ReadOnly ] IncrementalVelocityData velocity_data;
+	[ ShowInInspector, ReadOnly ] float velocity_current;
+
+    // Properties
+    public float CurrentSpeed => velocity_current;
+
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+    public void SetVelocityData( IncrementalVelocityData data )
+    {
+		velocity_data = data;
+	}
+
+    public void OnAcceleration()
+    {
+		velocity_current = Mathf.Max( 0, velocity_current );;
+
+		velocity_current = Mathf.Min( 
+			velocity_data.incremental_velocity_max, 
+			velocity_current + Time.deltaTime * velocity_data.incremental_velocity_max / velocity_data.incremental_velocity_max_duration 
+		);
+	}
+
+    public void Deceleration()
+    {
+		velocity_current = Mathf.Max( 
+            velocity_current - Time.deltaTime * velocity_data.incremental_velocity_decrease,
+            velocity_data.incremental_velocity_min
+        );
+    }
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
