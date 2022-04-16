bf07170baf382157ad87362c8fa6c0703c87ecf6
ofgonen <ofgonen@gmail.com>
2022-04-16 19:14:29 +0300

    MovementPath: Modify path points on Start
    - Modify path points to be world points
    - Create bolt_shaped prefab

--- Assets/Script/MovementPath.cs
+++ Assets/Script/MovementPath.cs
@@ -11,30 +11,43 @@ public class MovementPath : MonoBehaviour
 {
 #region Fields
   [ Title( "Setup" ) ]
+	public IntGameEvent event_movement;
 	public int path_index;
     public MovementPath_Set path_set;
-    public Vector3[] path_points;
+    public Vector3[] path_points; // Local points
 #endregion
 
 #region Properties
 #endregion
 
 #region Unity API
-	private void OnEnable()
-	{
-		path_set.AddDictionary( path_index, path_points );
-	}
-
 	private void OnDisable()
 	{
 		path_set.RemoveDictionary( path_index );
 	}
+
+	private void Start()
+	{
+		ModifyPath();
+		path_set.AddDictionary( path_index, path_points );
+	}
 #endregion
 
 #region API
+	public void OnNutTrigger()
+	{
+		event_movement.Raise( path_index );
+	}
 #endregion
 
 #region Implementation
+	void ModifyPath()
+	{
+		for( var i = 0; i < path_points.Length; i++ )
+		{
+			path_points[ i ] += transform.position;
+		}
+	}
 #endregion
 
 #region Editor Only
@@ -42,7 +55,12 @@ public class MovementPath : MonoBehaviour
 	[ Button() ]
 	void ImportPath()
 	{
-		path_points = GetComponent< DOTweenPath >().wps.ToArray();
+		path_points = new Vector3[ transform.childCount ];
+
+		for( var i = 0; i < transform.childCount; i++ )
+		{
+			path_points[ i ] = transform.GetChild( i ).localPosition;
+		}
 	}
 #endif
 #endregion
