6f5be3d33f983ac745484d3019dbd978233e13b3
ofgonen <ofgonen@gmail.com>
2022-04-19 17:17:38 +0300

    UpgradeSystem: Use ExtensionMethod variables for PlayerPref parameters

--- Assets/Script/UpgradeSystem.cs
+++ Assets/Script/UpgradeSystem.cs
@@ -3,6 +3,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FFStudio;
 using Sirenix.OdinInspector;
 
 [ CreateAssetMenu( fileName = "incremental_system", menuName = "FF/Data/Incremental/System" ) ]
@@ -24,62 +25,60 @@ public class UpgradeSystem : ScriptableObject
 #region API
     public void UnlockIncremental_Durability()
     {
-		var currentIndex = PlayerPrefs.GetInt( "durability_index", 0 );
+		var currentIndex = PlayerPrefs.GetInt( ExtensionMethods.durability_index, 0 );
 		currency.SharedValue -= incremental_durability.ReturnIncremental( currentIndex ).incremental_cost;
 
-		PlayerPrefs.SetInt( "durability_index", Mathf.Min( currentIndex + 1, incremental_durability.IncrementalCount ) );
+		PlayerPrefs.SetInt( ExtensionMethods.durability_index, Mathf.Min( currentIndex + 1, incremental_durability.IncrementalCount ) );
 	}
 
     public void UnlockIncremental_Velocity()
     {
-		var currentIndex = PlayerPrefs.GetInt( "velocity_index", 0 );
+		var currentIndex = PlayerPrefs.GetInt( ExtensionMethods.velocity_index, 0 );
 		currency.SharedValue -= incremental_velocity.ReturnIncremental( currentIndex ).incremental_cost;
 
-		PlayerPrefs.SetInt( "velocity_index", Mathf.Min( currentIndex + 1, incremental_velocity.IncrementalCount ) );
+		PlayerPrefs.SetInt( ExtensionMethods.velocity_index, Mathf.Min( currentIndex + 1, incremental_velocity.IncrementalCount ) );
 	}
 
     public void UnlockIncremental_Currency()
     {
-		var currentIndex = PlayerPrefs.GetInt( "currency_index", 0 );
+		var currentIndex = PlayerPrefs.GetInt( ExtensionMethods.currency_index, 0 );
 		currency.SharedValue -= incremental_curreny.ReturnIncremental( currentIndex ).incremental_cost;
 
-		PlayerPrefs.SetInt( "currency_index", Mathf.Min( currentIndex + 1, incremental_curreny.IncrementalCount ) );
+		PlayerPrefs.SetInt( ExtensionMethods.currency_index, Mathf.Min( currentIndex + 1, incremental_curreny.IncrementalCount ) );
 	}
-
-    public bool CanAfford_Durability()
-    {
-		return currency.SharedValue >= incremental_durability.ReturnIncremental( PlayerPrefs.GetInt( "durability_index", 0 ) ).incremental_cost;
-
-	}
-
-    public bool CanAfford_Velocity()
-    {
-		return currency.SharedValue >= incremental_velocity.ReturnIncremental( PlayerPrefs.GetInt( "velocity_index", 0 ) ).incremental_cost;
-    }
-
-    public bool CanAfford_Currency()
-    {
-		return currency.SharedValue >= incremental_curreny.ReturnIncremental( PlayerPrefs.GetInt( "currency_index", 0 ) ).incremental_cost;
-    }
-
-    public bool CanShow_Durability()
-    {
-		return PlayerPrefs.GetInt( "durability_index", 0 ) < incremental_durability.IncrementalCount - 1;
-
-	}
-
-    public bool CanShow_Velocity()
-    {
-		return PlayerPrefs.GetInt( "velocity_index", 0 ) < incremental_velocity.IncrementalCount - 1;
-    }
-
-    public bool CanShow_Currency()
-    {
-		return PlayerPrefs.GetInt( "currency_index", 0 ) < incremental_curreny.IncrementalCount - 1;
-    }
 #endregion
 
 #region Implementation
+    bool CanAfford_Durability()
+    {
+		return currency.SharedValue >= incremental_durability.ReturnIncremental( PlayerPrefs.GetInt( ExtensionMethods.durability_index, 0 ) ).incremental_cost;
+
+	}
+
+    bool CanAfford_Velocity()
+    {
+		return currency.SharedValue >= incremental_velocity.ReturnIncremental( PlayerPrefs.GetInt( ExtensionMethods.velocity_index, 0 ) ).incremental_cost;
+    }
+
+    bool CanAfford_Currency()
+    {
+		return currency.SharedValue >= incremental_curreny.ReturnIncremental( PlayerPrefs.GetInt( ExtensionMethods.currency_index, 0 ) ).incremental_cost;
+    }
+
+    bool CanShow_Durability()
+    {
+		return PlayerPrefs.GetInt( ExtensionMethods.durability_index, 0 ) < incremental_durability.IncrementalCount - 1;
+	}
+
+    bool CanShow_Velocity()
+    {
+		return PlayerPrefs.GetInt( ExtensionMethods.velocity_index, 0 ) < incremental_velocity.IncrementalCount - 1;
+    }
+
+    bool CanShow_Currency()
+    {
+		return PlayerPrefs.GetInt( ExtensionMethods.currency_index, 0 ) < incremental_curreny.IncrementalCount - 1;
+    }
 #endregion
 
 #region Editor Only
