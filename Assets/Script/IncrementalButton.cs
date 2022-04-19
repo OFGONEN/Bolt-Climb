68eca36b21e6df9497164678f156cfeef812f991
ofgonen <ofgonen@gmail.com>
2022-04-20 01:59:51 +0300

    Bugfix @IncrementalButton: Level text is wrong

--- Assets/Script/IncrementalButton.cs
+++ Assets/Script/IncrementalButton.cs
@@ -45,9 +45,9 @@ public class IncrementalButton : UIEntity
     {
 		ui_button.interactable = available;
 		this.available         = available;
-		ui_text_cost.text           = cost.ToString();
-		ui_text_cost.color          = color;
-
+		ui_text_cost.text      = cost.ToString();
+		ui_text_cost.color     = color;
+		ui_text_level.text     = "Level " + ( level + 1 );
 	}
 #endregion
 
