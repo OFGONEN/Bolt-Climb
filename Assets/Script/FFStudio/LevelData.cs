0755841aa8e5da8a20beff1401b24b41c1d7bf74
ofgonen <ofgonen@gmail.com>
2022-06-01 16:45:09 +0300

    LevelData: Set incrementals to a index instead of resetting

--- Assets/Script/FFStudio/LevelData.cs
+++ Assets/Script/FFStudio/LevelData.cs
@@ -14,7 +14,9 @@ namespace FFStudio
 	[ Title( "Setup" ) ]
 		[ ValueDropdown( "SceneList" ), LabelText( "Scene Index" ) ] public int scene_index;
         [ LabelText( "Override As Active Scene" ) ] public bool scene_overrideAsActiveScene;
-        [ LabelText( "Reset Incremental" ) ] public bool resetIncremental;
+        [ LabelText( "Set Incremental" ) ] public bool incremental_set;
+        [ LabelText( "Set Index of Velocity Incremental" ), ShowIf( "incremental_set" ) ] public int incremental_set_index_velocity;
+        [ LabelText( "Set Index of Durability Incremental" ), ShowIf( "incremental_set" ) ] public int incremental_set_index_durability;
 
 #if UNITY_EDITOR
 		private static IEnumerable SceneList()
