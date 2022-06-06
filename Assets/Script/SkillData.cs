3ff87700de9b41513adcaade5145c78653da4f24
ofgonen <ofgonen@gmail.com>
2022-06-06 15:51:56 +0300

    SkillData: Index, Value, Cost properties

--- Assets/Script/SkillData.cs
+++ Assets/Script/SkillData.cs
@@ -10,11 +10,30 @@ using FFStudio;
 public class SkillData : ScriptableObject
 {
 #region Fields
+
+  // Public
 	public SkillValue[] skill_value_array;
 	public float skill_value_default;
 	public Sprite skill_texture;
 	public string skill_description;
 	public string skill_key;
 	public UnityEvent skill_OnUnlock;
+
+  // Private
+	int skill_index;
+
+  // Properties
+	public bool IsUnlocked
+	{
+		get
+		{
+			skill_index = PlayerPrefsUtility.Instance.GetInt( skill_key, -1 );
+			return skill_index != -1;
+		}
+	}
+
+	public float Index => skill_index;
+	public float Value => skill_value_array[ skill_index ].value;
+	public float Cost => skill_value_array[ skill_index ].cost;
 #endregion
 }
