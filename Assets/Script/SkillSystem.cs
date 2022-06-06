8211b233f67f83830776c8ba370c864da3d29d60
ofgonen <ofgonen@gmail.com>
2022-06-06 14:58:01 +0300

    Create SkillSystem type
    - Create system asset
    - Call _OnAwake_ via asset manager

---
+++ Assets/Script/SkillSystem.cs
@@ -0,0 +1,34 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+
+[ CreateAssetMenu( fileName = "skill_system", menuName = "FF/Skill System" ) ]
+public class SkillSystem : ScriptableObject
+{
+#region Fields
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+    public void OnAwake()
+    {
+
+    }
+#endregion
+
+#region API
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
